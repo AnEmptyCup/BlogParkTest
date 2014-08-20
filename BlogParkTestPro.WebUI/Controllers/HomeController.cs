2e09a576cc5996eb46ce0877b2c6a9070e61a67e
AnEmptyCup <chenghang1937@hotmail.com>
2014-08-20 13:34:27 +0800

    准备登录页面

--- BlogParkTestPro.WebUI/Controllers/HomeController.cs
+++ BlogParkTestPro.WebUI/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BlogParkTestPro.WebUI.Models;
 
 namespace BlogParkTestPro.WebUI.Controllers
 {
@@ -10,23 +11,39 @@ namespace BlogParkTestPro.WebUI.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "";
 
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            ViewBag.Message = "";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "";
 
             return View();
+        }
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserModels model)
+        {
+            if (model.isnextautologin)
+                return RedirectToAction("Index");
+            else
+                return View();
         }
     }
 }
