14d03bd20a613efed0f50a4e627ef9ab73d5d58b
AnEmptyCup <chenghang1937@hotmail.com>
2014-08-21 14:53:12 +0800

    添加相关类

---
+++ BlogParkTestPro.DAL/MemberSimpleOperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogParkTestPro.DAL
+{
+    /// <summary>
+    /// 简单的对会员信息的操作类
+    /// </summary>
+    public class MemberSimpleOperation
+    {
+        //DbHelperSQL help=new DbHelperSQL();
+        //public 
+    }
+}
